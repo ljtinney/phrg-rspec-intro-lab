dde4fa5a418ea0c84f190c49d42347ebeee32f86
Liam Tinney <ltinney@powerhrg.com>
2020-06-29 16:15:11 -0400

    Done.

--- spec/baby_food_spec.rb
+++ spec/baby_food_spec.rb
@@ -7,11 +7,27 @@ RSpec.describe BabyFood do
     expect { BabyFood.new(kind: "peas", size: "small") }.to output("num nums\n").to_stdout
   end
 
+  it "puts 'WAAAAHHH!' for large peas" do
+    expect { BabyFood.new(kind: "peas", size: "large") }.to output("WAAAAHHH!\n").to_stdout
+  end
+
+  it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices" do
+    expect { BabyFood.new(kind: "banana slices", size: "small") }.to output("wwwwWWWWWAAAHHHHH!\n").to_stdout
+  end
+
+  it "puts 'num nums' for small carrots" do
+    expect { BabyFood.new(kind: "carrots", size: "small") }.to output("num nums\n").to_stdout
+  end
+
+  it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas" do
+    expect { BabyFood.new(kind: "peas", size: "small", texture: "hard") }.to output("Whhhaaaa WAAAAHHHHH!!\n").to_stdout
+  end
+
   # These tests are pending until they are passed a block. Follow the formula for 'small peas' to make these work.
-  it "puts 'WAAAAHHH!' for large peas"
-  it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices"
-  it "puts 'num nums' small carrots"
-  it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas"
+  # it "puts 'WAAAAHHH!' for large peas"
+  # it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices"
+  # it "puts 'num nums' small carrots"
+  # it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas"
 
   # Can you write one test using iteration to check all the FAVORITE_KINDS of baby food?
 
