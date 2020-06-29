8acb3f3383a0e924d817d80e38b71d8da8a4c413
Liam Tinney <ltinney@powerhrg.com>
2020-06-29 16:16:18 -0400

    Done.

--- spec/baby_food_spec.rb
+++ spec/baby_food_spec.rb
@@ -22,14 +22,4 @@ RSpec.describe BabyFood do
   it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas" do
     expect { BabyFood.new(kind: "peas", size: "small", texture: "hard") }.to output("Whhhaaaa WAAAAHHHHH!!\n").to_stdout
   end
-
-  # These tests are pending until they are passed a block. Follow the formula for 'small peas' to make these work.
-  # it "puts 'WAAAAHHH!' for large peas"
-  # it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices"
-  # it "puts 'num nums' small carrots"
-  # it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas"
-
-  # Can you write one test using iteration to check all the FAVORITE_KINDS of baby food?
-
-  # This spec file is complete when all favorite foods, invalid size, and invalid texture scenarios have been tested.
 end
